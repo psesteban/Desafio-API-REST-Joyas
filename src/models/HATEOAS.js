0b357fab86d17ad213a0933e2f3583bbfa64ff7d
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-06-13 22:57:35 -0400

    puliendo código

--- src/models/HATEOAS.js
+++ src/models/HATEOAS.js
@@ -1,6 +1,6 @@
 // import dotenv from 'dotenv'
 import format from 'pg-format'
-import pool from '../database/config.js'
+import data from './querys.js'
 
 // dotenv.config() // para ingresar url base
 
@@ -10,10 +10,10 @@ import pool from '../database/config.js'
 const findAll = async ({ limit = 5, page = 1, order_by: orderBy = 'stock_DESC' }) => {
   try {
     const countQuery = 'SELECT COUNT(*) FROM inventario;'
-    const { rows: countResult } = await pool.query(countQuery)
+    const countResult = await data(countQuery)
     const totalJoyas = parseInt(countResult[0].count, 10)
     const totalStockQuery = 'SELECT SUM(stock) AS total_stock FROM inventario;'
-    const { rows: totalStockResult } = await pool.query(totalStockQuery)
+    const totalStockResult = await data(totalStockQuery)
     const stockTotal = parseInt(totalStockResult[0].total_stock, 10)
 
     const [campo, direccion] = orderBy.split('_')
@@ -25,13 +25,14 @@ const findAll = async ({ limit = 5, page = 1, order_by: orderBy = 'stock_DESC' }
     }
 
     const offset = Math.abs(+page !== 0 ? page - 1 : 0) * limit
+    console.log(offset)
     const query = format('SELECT * FROM inventario ORDER BY %I %s LIMIT $1 OFFSET $2;', campo, direccion)
-
-    const { rows } = await pool.query(query, [limit, offset])
-    const results = rows.map((row) => ({
-      ...row,
-      name: `${row.nombre}`,
-      href: `joyas/joya/${row.id}`
+    const joyas = await data(query, [limit, offset])
+    console.log(joyas)
+    const results = joyas.map((joya) => ({
+      // ...row, // para devolver todo
+      name: `${joya.nombre}`,
+      href: `joyas/joya/${joya.id}`
     }))
 
     return {
